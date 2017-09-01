501a029bf165cdeb9ac60cd9396387021a092d2d
Ludovic Lerus <lerus_l@epitech.eu>
2017-09-01 18:13:48 +0200

    fix(index): fix moduleLoader name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const asyncMiddleware = require('./libs/asyncMiddleware');
-const moduleLoeader = require('./libs/moduleLoader');
+const moduleLoader = require('./libs/moduleLoader');
 const ModuleService = require('./libs/ModuleService');
 
 module.exports = {
 	asyncMiddleware,
-	moduleLoeader,
+	moduleLoader,
 	ModuleService,
 };
