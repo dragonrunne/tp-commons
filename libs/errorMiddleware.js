ee38f703072fd3a449bd4c0f6f2ed5358f443ffa
Ludovic Lerus <lerus_l@epitech.eu>
2017-09-04 17:42:19 +0200

    feat(errorMiddleware): joi errors handled

--- libs/errorMiddleware.js
+++ libs/errorMiddleware.js
@@ -1,7 +1,9 @@
 const { SevenBoom } = require('graphql-apollo-errors');
 
 module.exports = (logger) => (err, req, res, next) => { // eslint-disable-line no-unused-vars
-	if (err.name === 'ValidationError') {
+	if (err.isJoi) {
+		err = SevenBoom.badRequest(err.message, err.details, err.name);
+	} else if (err.name === 'ValidationError') {
 		err = SevenBoom.badImplementation(err.message, err.errors, err.name);
 	} else if (!err.isBoom) {
 		logger.error(err.stack);
