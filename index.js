b58f1877761b567f944e70f75b108912d172437a
Ludovic Lerus <lerus_l@epitech.eu>
2017-09-28 19:08:51 +0200

    feat(csv): first version

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const moduleLoader = require('./libs/moduleLoader');
 const ModuleService = require('./libs/ModuleService');
 const Mongoose = require('./libs/Mongoose');
 const joiSchemas = require('./libs/joiSchemas');
+<<<<<<< 62d13a9859a5b1cb15854495ca1bc33284ea6c7f
 const CloudinaryStorage = require('./libs/storages/cloudinary.storage');
 const FsStorage = require('./libs/storages/fs.storage');
 const Storage = require('./libs/storages/storage');
+=======
+const CSV = require('./libs/CSV');
+>>>>>>> feat(csv): first version
 
 module.exports = {
 	asyncMiddleware,
@@ -15,7 +19,11 @@ module.exports = {
 	ModuleService,
 	Mongoose,
 	joiSchemas,
+<<<<<<< 62d13a9859a5b1cb15854495ca1bc33284ea6c7f
 	CloudinaryStorage,
 	FsStorage,
 	Storage,
+=======
+	CSV,
+>>>>>>> feat(csv): first version
 };
