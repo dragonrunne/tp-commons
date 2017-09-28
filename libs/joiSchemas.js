58964ebdaac1b9f6c5935c08be076c3feda7db6f
Ludovic Lerus <lerus_l@epitech.eu>
2017-09-28 18:33:48 +0200

    feat(joiSchemas): joi, date, language and picture added

--- libs/joiSchemas.js
+++ libs/joiSchemas.js
@@ -2,11 +2,15 @@ const joi = require('joi');
 const mongoose = require('mongoose');
 
 module.exports = {
+	joi,
 	NUMBER:    joi.number().min(0),
 	STRING:    joi.string().min(1),
 	ARRAY:     joi.array(),
 	OBJECT:    joi.object(),
 	BOOLEAN:   joi.boolean(),
+	DATE:      joi.date(),
+	LANGUAGE:  joi.string().min(2).regex(/^[a-z]{2}$/),
+	PICTURE:   joi.any(),
 	OBJECT_ID: joi.extend((j) => ({
 		base:     j.string(),
 		name:     'objectId',
