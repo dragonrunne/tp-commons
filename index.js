50d618f432524546e07fc06d3c33d34b08da1cee
Ludovic Lerus <lerus_l@epitech.eu>
2017-10-02 16:02:04 +0200

    fix(pkg): fix conflicts and unused modules

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ const moduleLoader = require('./libs/moduleLoader');
 const ModuleService = require('./libs/ModuleService');
 const Mongoose = require('./libs/Mongoose');
 const joiSchemas = require('./libs/joiSchemas');
-<<<<<<< 62d13a9859a5b1cb15854495ca1bc33284ea6c7f
 const CloudinaryStorage = require('./libs/storages/cloudinary.storage');
 const FsStorage = require('./libs/storages/fs.storage');
 const Storage = require('./libs/storages/storage');
-<<<<<<< b58f1877761b567f944e70f75b108912d172437a
-=======
 const CSV = require('./libs/CSV');
->>>>>>> feat(csv): first version
-=======
-const CSV = require('./libs/CSV');
->>>>>>> feat(csv): first version
 
 module.exports = {
 	asyncMiddleware,
@@ -23,15 +16,8 @@ module.exports = {
 	ModuleService,
 	Mongoose,
 	joiSchemas,
-<<<<<<< 62d13a9859a5b1cb15854495ca1bc33284ea6c7f
 	CloudinaryStorage,
 	FsStorage,
 	Storage,
-<<<<<<< b58f1877761b567f944e70f75b108912d172437a
-=======
 	CSV,
->>>>>>> feat(csv): first version
-=======
-	CSV,
->>>>>>> feat(csv): first version
 };
