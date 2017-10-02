1ac41ab1535f7330f87d2524dc5882a49bb15e7c
Ludovic Lerus <lerus_l@epitech.eu>
2017-10-02 15:58:14 +0200

    feat(CSV): parse method ok

--- libs/CSV.js
+++ libs/CSV.js
@@ -1,20 +1,55 @@
+const _ = require('lodash');
 const csv = require('fast-csv');
+const Joi = require('joi');
+const { Duplex } = require('stream');
 
 class CSV {
-	static parse(stream, delimiter = ';') {
+	static bufferToStream(buffer) {
+		const stream = new Duplex();
+		stream.push(buffer);
+		stream.push(null);
+		return stream;
+	}
+
+	static isBuffer(buffer) {
+		return buffer instanceof Buffer;
+	}
+
+	static parse(stream, { joiSchema, delimiter = ',', transformer = (d) => d }) {
 		const options = {
 			delimiter,
 			headers: true,
 		};
 		const records = [];
+		const errors = [];
+		let currentLine = 0;
 
-		return new Promise((resolve) => {
+		if (CSV.isBuffer(stream)) {
+			stream = CSV.bufferToStream(stream);
+		}
+
+		return new Promise((resolve, reject) => {
 			stream
 				.pipe(csv(options))
 				.on('data', (data) => {
-					records.push(data);
+					++currentLine;
+					data = _.mapValues(data, (value) => (value === '' ? null : value));
+					data = transformer(data);
+					Joi.validate(data, joiSchema, (err, value) => {
+						if (err) {
+							errors.push({
+								line:  currentLine,
+								error: err,
+							});
+						}
+						records.push(value);
+					});
 				})
 				.on('end', () => {
+					if (errors.length) {
+						reject(errors);
+						return;
+					}
 					resolve(records);
 				});
 		});
