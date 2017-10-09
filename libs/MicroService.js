aee152cc09a782fe89bd327e0a9f2b71c76c2734
Ludovic Lerus <lerus_l@epitech.eu>
2017-10-09 18:44:56 +0200

    feat(MicroService): initMethod and mongoURI are now optional

--- libs/MicroService.js
+++ libs/MicroService.js
@@ -5,22 +5,28 @@ class MicroService {
 		this.options = options;
 	}
 
-	_init(initMethod = null) {
+	async _init(initMethod = null) {
 		if (!initMethod) {
 			return Promise.resolve();
 		}
-		return initMethod();
+		await initMethod();
+		logger.info('[init] Initialialization finished');
+		return Promise.resolve();
 	}
 
-	_connectMongo(mongoURI) {
-		return Mongoose.connect(mongoURI);
+	async _connectMongo(mongoURI = null) {
+		if (!mongoURI) {
+			return Promise.resolve();
+		}
+
+		await Mongoose.connect(mongoURI);
+		logger.info('[mongo] Connection to the database OK');
+		return Promise.resolve();
 	}
 
 	async start(app) {
 		await this._connectMongo(this.options.mongoURI);
-		logger.info('[mongo] Connection to the database OK');
 		await this._init(this.options.initMethod);
-		logger.info('[init] Initialialization finished');
 
 		return new Promise((resolve) => {
 			app.listen(this.options.port, () => {
