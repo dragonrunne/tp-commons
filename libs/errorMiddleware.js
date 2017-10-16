582ed704518bd18508683442b79d71573a996344
Ludovic Lerus <lerus_l@epitech.eu>
2017-10-16 18:16:10 +0200

    feat(errorMiddleware): unauthorized handled

--- libs/errorMiddleware.js
+++ libs/errorMiddleware.js
@@ -5,6 +5,8 @@ module.exports = (logger) => (err, req, res, next) => { // eslint-disable-line n
 		err = SevenBoom.badRequest(err.message, err.details, err.name);
 	} else if (err.name === 'ValidationError') {
 		err = SevenBoom.badImplementation(err.message, err.errors, err.name);
+	} else if (err.name === 'UnauthorizedError') {
+		err = SevenBoom.unauthorized(err.message, err.inner, err.name);
 	} else if (!err.isBoom) {
 		logger.error(err.stack);
 		err = SevenBoom.badImplementation(err.message, err.stack, err.name);
