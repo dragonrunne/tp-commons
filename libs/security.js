83d2b39ef0432eee8c05f57157e6d8100574e73c
Guillaume Galas <guillaume.galas@protonmail.com>
2018-10-04 12:02:36 +0200

    feat(security): Adding CORS security

---
+++ libs/security.js
@@ -0,0 +1,21 @@
+const cors = require('cors');
+
+const whitelist = [
+	'http://127.0.0.1',
+	'http://localhost',
+].concat(process.env.WHITELIST_URLS ? process.env.WHITELIST_URLS.split(',') : []);
+
+const corsOptions = {
+	origin(origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+module.exports = {
+	CORS: cors(corsOptions),
+};
