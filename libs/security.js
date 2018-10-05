bd2a04a6cc1a10e9fc7d92200036b6eca5dab166
Guillaume Galas <guillaume.galas@protonmail.com>
2018-10-05 11:16:26 +0200

    feat(security): CORS should not use env

--- libs/security.js
+++ libs/security.js
@@ -4,28 +4,32 @@ const whitelist = [
 	'http:\/\/127.0.0.1:8080',
 	'http:\/\/localhost:8080',
 	'http:\/\/localhost:8081',
-].concat(process.env.WHITELIST_URLS ? process.env.WHITELIST_URLS.split(',') : []);
+];
 
-if (process.env.SUPER_SECRET_PASSPHRASE) {
-	whitelist.push(process.env.SUPER_SECRET_PASSPHRASE);
+function initCors(options) {
+	whitelist.concat(options.whitelist_urls ? options.whitelist_urls.split(',') : []);
+
+	if (options.secret) {
+		whitelist.push(options.secret);
+	}
+
+	return cors({
+		origin(origin, next) {
+			const valids = whitelist.map((url) => {
+				const regex = new RegExp(url);
+				return regex.test(origin);
+			});
+			if (valids.indexOf(true) !== -1) {
+				next(null, true);
+			} else {
+				next(new Error('Authorization denied'));
+			}
+		},
+		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+		preflightContinue:    false, // Does not let the OPTIONS requests continue to the next handlers
+	});
 }
 
-const corsOptions = {
-	origin(origin, next) {
-		const valids = whitelist.map((url) => {
-			const regex = new RegExp(url);
-			return regex.test(origin);
-		});
-		if (valids.indexOf(true) !== -1) {
-			next(null, true);
-		} else {
-			next(new Error('Authorization denied'));
-		}
-	},
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-	preflightContinue:    false, // Does not let the OPTIONS requests continue to the next handlers
-};
-
 module.exports = {
-	CORS: cors(corsOptions),
+	CORS: (options) => initCors(options),
 };
