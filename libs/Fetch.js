4520c06eff67fa26887af7a25aa2e3b753035336
Guillaume Galas <guillaume.galas@protonmail.com>
2018-10-12 16:07:21 +0200

    feat(fetch): Improving Fetch by adding authenticate method and defaultheaders

--- libs/Fetch.js
+++ libs/Fetch.js
@@ -3,7 +3,6 @@ const { URL, URLSearchParams } = require('url');
 const FormData = require('form-data');
 
 const defaultHeader = { 'Content-Type': 'application/json' };
-const multipartHeader = { 'Content-Type': undefined };
 
 if (!global.logger) {
 	global.logger = {
@@ -17,7 +16,16 @@ if (!global.logger) {
  * A wrapper around node-fetch to handle JSON and errors.
  */
 class Fetch {
-	static generateHeaders(headers) {
+
+	static set defaultHeader(headers) {
+		return Object.assign({ ...defaultHeader }, headers);
+	}
+
+	static get defaultHeader() {
+		return defaultHeader;
+	}
+
+	static _generateHeaders(headers) {
 		const microServiceHeaders = {};
 
 		if (headers.authorization || headers.Authorization) {
@@ -39,6 +47,11 @@ class Fetch {
 		return microServiceHeaders;
 	}
 
+	static async authenticate(apiKey, identity) {
+		const token = await Fetch.post({}, `${microServices[identity].url}`, { apiKey });
+		defaultHeaders.Authorization = `Bearer ${token}`;
+	}
+
 	static get(headers, url, params = null) {
 		const ObjectURL = new URL(url);
 
@@ -49,7 +62,7 @@ class Fetch {
 
 		return fetch(ObjectURL.href, {
 			method:  'GET',
-			headers: Fetch.generateHeaders(headers),
+			headers: Fetch._generateHeaders(headers),
 			timeout: 0,
 		})
 			.then((res) => res.json())
@@ -66,7 +79,7 @@ class Fetch {
 		return fetch(url, {
 			method,
 			body:    body ? JSON.stringify(body) : undefined,
-			headers: headers || defaultHeader,
+			headers: headers || { ...defaultHeader },
 			timeout: 0,
 		})
 			.then(async (res) => {
@@ -83,7 +96,7 @@ class Fetch {
 		return fetch(url, {
 			method:  'POST',
 			body:    JSON.stringify(body),
-			headers: Object.assign(defaultHeader, Fetch.generateHeaders(headers)),
+			headers: Object.assign({ ...defaultHeader }, Fetch._generateHeaders(headers)),
 			timeout: 0,
 		})
 			.then((res) => res.json())
@@ -100,7 +113,7 @@ class Fetch {
 		return fetch(url, {
 			method:  'PUT',
 			body:    JSON.stringify(body),
-			headers: Object.assign(defaultHeader, Fetch.generateHeaders(headers)),
+			headers: Object.assign({ ...defaultHeader }, Fetch._generateHeaders(headers)),
 			timeout: 0,
 		})
 			.then((res) => res.json())
@@ -116,7 +129,7 @@ class Fetch {
 	static delete(headers, url) {
 		return fetch(url, {
 			method:  'DELETE',
-			headers: Fetch.generateHeaders(headers),
+			headers: Fetch._generateHeaders(headers),
 			timeout: 0,
 		})
 			.then((res) => res.json())
